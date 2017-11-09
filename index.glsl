ccbf0d3d4def959390996398ac4e86753f5e2a27
Honigstein Samuel <samuel.honigstein@gmail.com>
2017-11-09 14:08:26 -0500

    correct

--- index.glsl
+++ index.glsl
@@ -3,7 +3,7 @@ vec2 screenspace(mat4 projectionmatrix, mat4 modelviewmatrix, vec3 position){
   vec4 temp = projectionmatrix * modelviewmatrix * vec4(position, 1.0);
   temp.xyz /= temp.w;
   temp.xy = (0.5)+(temp.xy)*0.5;
-  return temp.xy
+  return temp.xy;
 }
 
 
